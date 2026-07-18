4b14cc48338bf581f8c7a89b84bbdcad18561344
Rudra Wakode <your_email@gmail.com>
2026-07-19 00:13:51 +0530

    Complete user registration with React, FastAPI and PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,35 @@
-from app.routes.career_roadmap import router as career_roadmap_router
-from app.routes.gemini_test import router as gemini_router
-from app.routes.mock_interview import router as mock_interview_router
-from app.routes.interview import router as interview_router
-from app.routes.cover_letter import router as cover_letter_router
-from app.routes.job import router as job_router
-from app.routes.resume import router as resume_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import Base, engine
 from app.models import User, Profile, Resume
+
 from app.routes.auth import router as auth_router
 from app.routes.profile import router as profile_router
+from app.routes.resume import router as resume_router
+from app.routes.job import router as job_router
+from app.routes.cover_letter import router as cover_letter_router
+from app.routes.interview import router as interview_router
+from app.routes.mock_interview import router as mock_interview_router
+from app.routes.gemini_test import router as gemini_router
+from app.routes.career_roadmap import router as career_roadmap_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="AI Career Copilot",
     version="1.0.0"
+)
+
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
